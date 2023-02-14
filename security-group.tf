98ad03b779753533432e232d39857241228393de
monda <mondaronnie@gmail.com>
2023-02-14 06:18:42 +0300

    updated security group

--- security-group.tf
+++ security-group.tf
@@ -2,25 +2,29 @@ resource "aws_security_group" "web-server" {
     name = "allow-ssh-web-server"
     description = "allow ssh traffic web server"
     vpc_id = aws_vpc.main-VPC.id
-    
 
-    ingress = {
+
+    ingress = [ {
         from_port = 22
         to_port   = 22
         protocol  = "tcp"
 
     cidr_blocks = ["0.0.0.0/0"]
     
-    ingress = {
+   
+    } ]
+
+    Ingress = [ {
         from_port = 80
         to_port   = 80
         protocol  = "tcp"
 
     cidr_blocks = ["0.0.0.0/0"]
-    
-    }
+    } ]
+
 }
-}
+        
+
 
 
 
